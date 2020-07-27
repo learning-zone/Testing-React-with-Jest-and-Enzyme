29d7d587df60d9888d1ea66061f2da43bbb536fc
Pradeep Kumar <pradeep.vwa@gmail.com>
2020-07-27 22:57:35 +0530

    Check Prop Types

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import Header from './Component/Header/Header'
 import Headline from './Component/Headline/Headline'
 import './App.scss'
 
+const tempArr = [{
+  fName: 'Alex',
+  lName: 'K',
+  email: 'alexk@gmail.com',
+  age: 24,
+  onlineStatus: true
+}]
+
 function App() {
   return (
     <div className="App" data-test="AppComponent">
       <Header />
       <section className="main">
-        <Headline header="Posts" desc="Click the button to render the posts!" />
+        <Headline header="Posts" desc="Click the button to render the posts!" tempArr={tempArr} />
       </section>
     </div>
   );
