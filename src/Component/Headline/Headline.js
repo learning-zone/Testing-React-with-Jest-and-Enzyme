f717143cd509423943cfd307245b39301e1726cd
Pradeep Kumar <pradeep.vwa@gmail.com>
2020-07-27 18:46:24 +0530

    Prop Types

--- src/Component/Headline/Headline.js
+++ src/Component/Headline/Headline.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class Headline extends Component {
 
@@ -19,6 +20,11 @@ class Headline extends Component {
             </div>
         )
     }
+}
+
+Headline.propTypes = {
+    header: PropTypes.string,
+    desc: PropTypes.string
 }
 
 export default Headline
