a0345474aac43ad0a2c0df07a7fa91433eb05e0a
Pradeep Kumar <pradeep.vwa@gmail.com>
2020-08-02 11:55:46 +0530

    Updates

---
+++ precache-manifest.7b0c233cddd76111d69af99ccdbba881.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd52d50ba067ba1ddac52870483ba3d3",
+    "url": "/testing-react-with-jest-and-enzyme/index.html"
+  },
+  {
+    "revision": "c35990ffe3d338d3dbbb",
+    "url": "/testing-react-with-jest-and-enzyme/static/css/main.6c910275.chunk.css"
+  },
+  {
+    "revision": "dc2b3b82176e35315355",
+    "url": "/testing-react-with-jest-and-enzyme/static/js/2.dbe70255.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/testing-react-with-jest-and-enzyme/static/js/2.dbe70255.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c35990ffe3d338d3dbbb",
+    "url": "/testing-react-with-jest-and-enzyme/static/js/main.e9bb8019.chunk.js"
+  },
+  {
+    "revision": "4092db04703a18c6c347",
+    "url": "/testing-react-with-jest-and-enzyme/static/js/runtime-main.15a9d36e.js"
+  }
+]);
